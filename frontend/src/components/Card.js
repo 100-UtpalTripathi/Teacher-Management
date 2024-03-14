b99a5086dbe085beef71a5332b8f4961e32ad804
100-UtpalTripathi <utpaltripathi14@gmail.com>
2024-03-14 11:17:52 +0530

    Fixed errors in integration of update Records

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -82,7 +82,7 @@ const Card = (props) => {
           <div className="modal-content">
             <div className="modal-header">
               <h1 className="modal-title fs-5" id={`staticBackdropLabel-${props.teacher.id}`}>
-                Modal title
+                Update Teacher Record
               </h1>
               <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
