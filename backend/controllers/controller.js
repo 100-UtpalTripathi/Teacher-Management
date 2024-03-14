bb9af29ae7671ab6ce4281619d200de5ca2f2ad1
100-UtpalTripathi <utpaltripathi14@gmail.com>
2024-03-15 01:54:34 +0530

    FileStorage Error Fixed!

--- backend/controllers/controller.js
+++ backend/controllers/controller.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import uniqid from "uniqid";
 
-const dataFilePath = "../frontend/build/teachers.json";
+const dataFilePath = "./teachers.json";
 
 // Function to read data from the file asynchronously
 export const readDataFromFile = async () => {
